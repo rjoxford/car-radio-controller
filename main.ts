445d578d2a21bd198876bfcb984ca7f154a337e6
rjoxford <r.j.oxford@hotmail.co.uk>
2022-12-14 13:06:33 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,12 +7,9 @@ basic.showLeds(`
     # # # # #
     . # . # .
     `)
-loops.everyInterval(100, function () {
+loops.everyInterval(10, function () {
     forward = input.rotation(Rotation.Pitch)
     turn = input.rotation(Rotation.Roll)
-    if (Math.abs(forward) >= 10) {
-        radio.sendValue("forward", Math.map(forward, -90, 90, -100, 100))
-    } else {
-        radio.sendValue("turn", Math.map(turn, -90, 90, -100, 100))
-    }
+    radio.sendValue("forward", Math.map(forward, -90, 90, -100, 100))
+    radio.sendValue("turn", Math.map(turn, -90, 90, -100, 100))
 })
